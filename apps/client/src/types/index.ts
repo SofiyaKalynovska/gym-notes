86a743cf04d6f7a037de1857cbc3cab8aef25905
Sofiya Kalynovska <sofia.k@oxcompany.io>
2025-07-18 17:52:29 +0300

    Added MuscleGroup type

--- apps/client/src/types/index.ts
+++ apps/client/src/types/index.ts
@@ -1,29 +1,38 @@
 export interface Exercise {
-  _id: string
-  name: string
-  muscleGroup?: string
-  description?: string
-  createdAt?: string
-  updatedAt?: string
+  _id: string;
+  name: string;
+  muscleGroup?: string;
+  description?: string;
+  createdAt?: string;
+  updatedAt?: string;
 }
 
 export interface WorkoutSet {
-  reps: number
-  weight: number
+  reps: number;
+  weight: number;
 }
 
 export interface WorkoutExercise {
-  exercise: Exercise | string  
-  sets: WorkoutSet[]
+  exercise: Exercise | string;
+  sets: WorkoutSet[];
 }
 
 export interface WorkoutSession {
-  _id: string
-  date: string
-  status: 'draft' |'active' | 'completed'
-  startTime: Date | null
-  duration?: string
-  exercises: WorkoutExercise[]
-  createdAt?: string
-  updatedAt?: string
+  _id: string;
+  date: string;
+  status: 'draft' | 'active' | 'completed';
+  startTime: Date | null;
+  duration?: string;
+  exercises: WorkoutExercise[];
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+export interface MuscleGroup {
+  _id: string;
+  name: string;
+  slug: string;
+  icon: string;
+  createdAt: string;
+  updatedAt: string;
 }
